41c71e3f6776453d0bb3449556931ef8e8d47e9b
Michael Cabelin <ncabelin@gmail.com>
2017-01-07 00:53:50 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# wiki_viewer
-Wikipedia Viewer
+# Wikipedia Viewer
+
+## Synopsis
+
+This app lets you query Wikipedia and returns a list of search results with a summary. It also can generate a random wikipedia article as an option.
+It demonstrates the use of the Wikipedia API using AJAX calls.
+
+The app is live at http://cabelin.com/wiki_viewer
+
+![wiki_viewer](https://cloud.githubusercontent.com/assets/15892944/14010784/9bf0d27c-f155-11e5-8455-066293c4f734.jpg)
+
+## Features
+
+The app uses jQuery AJAX, HTML5/CSS3, Bootstrap
+
+## Contributors
+
+Neptune Michael Cabelin
+
+## License
+
+MIT
---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $("#search").click(function() {
+    $("#results").empty();
+    var s = document.getElementById("searchWord").value;
+    var u = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + s + "&limit=20&namespace=0&format=jsonfm";
+    console.log(s);
+    $.ajax({
+        url: u,
+        type: 'GET',
+        dataType: 'jsonp',
+				data: {
+						format:"json"
+				},
+    }).success(function(data) {
+      console.log(data);
+			for (var i = 0; i < data[1].length; i++) {
+					$("#results").append("<div class='well positioned'><h3>"+data[1][i]+"</h3><p>"+data[2][i]+"</p><p><a href='"+data[3][i]+"'>more...</a></p>");	
+			};
+    });
+  });
+ });
